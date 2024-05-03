56f20c1dbba5a0ef1a832a98f5472af2fdfd31d3
nersiarnak <arnonersi777@gmail.com>
2024-05-03 16:45:36 +0300

    Готовая первая работа -1

---
+++ HomeWork/HomeWorkSkutorenko1.playground/Contents.swift
@@ -0,0 +1,24 @@
+//Создать характеристику студентов с помощью констант
+
+let nameСonstant = "Имя - "
+var nameStudent : String
+nameStudent = "Arnak"
+
+let serNameConstant = "Фамилия - "
+var serNameStudent : String
+serNameStudent = "Nersisyan"
+
+let nameUniversityConstant = "Название университета - "
+var nameUniversityStudent : String
+nameUniversityStudent = "MIREA"
+
+let numberKursConstant = "Курс - "
+var numberKursStudent : Int
+numberKursStudent = 2
+
+let numberGruppaConstant = "Номер группы - "
+var numberGruppaStudent : String
+numberGruppaStudent = "ИКБО-27-22"
+print("Характеристика студента:\n")
+print("\(nameСonstant)\(nameStudent)\n\(serNameConstant)\(serNameStudent)\n\(nameUniversityConstant)\(nameUniversityStudent)\n\(numberKursConstant)\(numberKursStudent)\n\(numberGruppaConstant)\(numberGruppaStudent)")
+
